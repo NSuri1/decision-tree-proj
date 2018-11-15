bff39352463dd09fb4b9fe67624c9aadfbe8de25
Macbook <nalinsuri@gmail.com>
2018-11-14 19:20:11 -0500

    Implement some steps of algorithm

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Node {
 
@@ -7,6 +12,7 @@ public class Node {
 	private String[] attributeList;
 	private String attributeToPredict;
 	private String decidedClass;
+	private String splittingCriterion;
 	private Map<String, Node> nextLevel;
 
 	public Node(Map<String, String>[] dataSet, String[] attributeList) {
@@ -14,44 +20,69 @@ public class Node {
 		this.attributeList = attributeList;
 		this.attributeToPredict = attributeList[attributeList.length - 1];
 		this.decidedClass = null;
+		this.splittingCriterion = null;
 		this.nextLevel = null;
 		generateDecisionTree();
 	}
 	
 	public void generateDecisionTree() {
+		// if all tuples of same class, label with class C
 		if (this.tuplesAreOfSameClass()) {
-			
+			this.decidedClass = dataSet[0].get(attributeToPredict);
+			return;
 		}
+		// if attribute list is 0, label with majority class in dataSet
+		if (attributeList.length == 0) {
+			this.decidedClass = getMajorityClass();
+			return;
+		}
+		// apply attribute selection
+		// get splitting criterion and remove from attribute list
+		this.splittingCriterion = attributeSelectionMethod();
+		attributeList = Arrays.stream(attributeList)
+						.filter(x -> x != splittingCriterion)
+						.toArray(String[]::new);
+		
+		// need to split data and create next levels
 	}
 
 	public boolean tuplesAreOfSameClass() {
-		for (int i = 0; i < dataSet.length - 1; i++) {
-			if (dataSet[i].get(attributeToPredict) != dataSet[i + 1].get(attributeToPredict)) {
-				return false;
+		String[] attributeLabels = Arrays.stream(dataSet)
+									.map(x -> x.get(attributeToPredict))
+									.toArray(String[]::new);
+		long countOfUniqueLabels = Arrays.stream(attributeLabels)
+									.distinct().count();
+		return countOfUniqueLabels == 1;
+	}
+	
+	public String getMajorityClass() {
+		String[] attributeLabels = Arrays.stream(dataSet)
+									.map(x -> x.get(attributeToPredict))
+									.toArray(String[]::new);
+		
+		List<String> labelsAsList = Arrays.asList(attributeLabels);
+		Set<String> mySet = new HashSet<String>(labelsAsList);
+		int countMajority = 0;
+		String majorityClassLabel = "";
+
+		for (String label: mySet) {
+			int count = Collections.frequency(labelsAsList, label);
+			if (count > countMajority) {
+				countMajority = count;
+				majorityClassLabel = label;
 			}
 		}
 
-		return true;
+		return majorityClassLabel;
 	}
 
-	/*
-	 * (2) if tuples in D are all of the same class, C, then (3) return N as a leaf
-	 * node labeled with the class C; (4) if attribute list is empty then (5) return
-	 * N as a leaf node labeled with the majority class in D; // majority voting (6)
-	 * apply Attribute selection method(D, attribute list) to find the “best”
-	 * splitting criterion; (7) label node N with splitting criterion; (8) if
-	 * splitting attribute is discrete-valued and multiway splits allowed then //
-	 * not restricted to binary trees (9) attribute list attribute list 􀀀 splitting
-	 * attribute; // remove splitting attribute (10) for each outcome j of splitting
-	 * criterion // partition the tuples and grow subtrees for each partition (11)
-	 * let Dj be the set of data tuples in D satisfying outcome j; // a partition
-	 * (12) if Dj is empty then (13) attach a leaf labeled with the majority class
-	 * in D to node N; (14) else attach the node returned by Generate decision
-	 * tree(Dj , attribute list) to node N; endfor (15) return N;
-	 * 
-	 */
-
 	public String attributeSelectionMethod() {
+		// Assuming discrete valued attributes and 
+		// using information gain to select splitting attribute
+		
+		// calculate information gain for all attributes
+		// select attribute with highest gain
+		
 		return "";
 	}
 
