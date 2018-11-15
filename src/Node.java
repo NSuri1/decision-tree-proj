ab0dd57f0a461094b2123b919b90c6d704e38666
Macbook <nalinsuri@gmail.com>
2018-11-14 22:20:04 -0500

    Add info gain algo... hasn't been tested yet.

--- src/Node.java
+++ src/Node.java
@@ -24,7 +24,7 @@ public class Node {
 		this.nextLevel = null;
 		generateDecisionTree();
 	}
-	
+
 	public void generateDecisionTree() {
 		// if all tuples of same class, label with class C
 		if (this.tuplesAreOfSameClass()) {
@@ -39,33 +39,26 @@ public class Node {
 		// apply attribute selection
 		// get splitting criterion and remove from attribute list
 		this.splittingCriterion = attributeSelectionMethod();
-		attributeList = Arrays.stream(attributeList)
-						.filter(x -> x != splittingCriterion)
-						.toArray(String[]::new);
-		
+		attributeList = Arrays.stream(attributeList).filter(x -> x != splittingCriterion).toArray(String[]::new);
+
 		// need to split data and create next levels
 	}
 
 	public boolean tuplesAreOfSameClass() {
-		String[] attributeLabels = Arrays.stream(dataSet)
-									.map(x -> x.get(attributeToPredict))
-									.toArray(String[]::new);
-		long countOfUniqueLabels = Arrays.stream(attributeLabels)
-									.distinct().count();
+		String[] attributeLabels = Arrays.stream(dataSet).map(x -> x.get(attributeToPredict)).toArray(String[]::new);
+		long countOfUniqueLabels = Arrays.stream(attributeLabels).distinct().count();
 		return countOfUniqueLabels == 1;
 	}
-	
+
 	public String getMajorityClass() {
-		String[] attributeLabels = Arrays.stream(dataSet)
-									.map(x -> x.get(attributeToPredict))
-									.toArray(String[]::new);
-		
+		String[] attributeLabels = Arrays.stream(dataSet).map(x -> x.get(attributeToPredict)).toArray(String[]::new);
+
 		List<String> labelsAsList = Arrays.asList(attributeLabels);
 		Set<String> mySet = new HashSet<String>(labelsAsList);
 		int countMajority = 0;
 		String majorityClassLabel = "";
 
-		for (String label: mySet) {
+		for (String label : mySet) {
 			int count = Collections.frequency(labelsAsList, label);
 			if (count > countMajority) {
 				countMajority = count;
@@ -77,13 +70,61 @@ public class Node {
 	}
 
 	public String attributeSelectionMethod() {
-		// Assuming discrete valued attributes and 
+		// Assuming discrete valued attributes and
 		// using information gain to select splitting attribute
-		
-		// calculate information gain for all attributes
+
+		// Calculate Info(D)
+		double totalEntropy = calculateEntropy(attributeToPredict, dataSet);
+
+		double maxGain = 0;
+		String attributeSelected = "";
+
+		// Calculate Info_Attr(D) for each attribute
+		// Calculate Info Gain using these two and pick attribute with max
+		for (String attribute : attributeList) {
+			double infoAttribute = 0;
+			String[] attributeClasses = Arrays.stream(dataSet).map(x -> x.get(attribute)).toArray(String[]::new);
+			List<String> classesAsList = Arrays.asList(attributeClasses);
+			Set<String> classSet = new HashSet<String>(classesAsList);
+			for (String label : classSet) {
+				// Figure out a better way to fix this warning
+				@SuppressWarnings("unchecked")
+				Map<String, String>[] relevantDataSet = Arrays.stream(dataSet).filter(x -> x.get(attribute) == label)
+						.toArray(HashMap[]::new);
+				double frequencyOfLabel = ((double) Collections.frequency(classesAsList, label))
+						/ attributeClasses.length;
+				infoAttribute += frequencyOfLabel * calculateEntropy(attributeToPredict, relevantDataSet);
+			}
+
+			double gain = totalEntropy - infoAttribute;
+			if (gain > maxGain) {
+				maxGain = gain;
+				attributeSelected = attribute;
+			}
+		}
+
 		// select attribute with highest gain
-		
-		return "";
+		return attributeSelected;
+	}
+
+	public double calculateEntropy(String entropyAttribute, Map<String, String>[] data) {
+		double entropy = 0;
+		String[] attributeToPredictLabels = Arrays.stream(data).map(x -> x.get(entropyAttribute))
+				.toArray(String[]::new);
+		List<String> labelsAsList = Arrays.asList(attributeToPredictLabels);
+		Set<String> classSet = new HashSet<String>(labelsAsList);
+
+		for (String label : classSet) {
+			int countOfLabel = Collections.frequency(labelsAsList, label);
+			double probabilityOfLabel = ((double) countOfLabel) / attributeToPredictLabels.length;
+			entropy -= probabilityOfLabel * logBase2(probabilityOfLabel);
+		}
+
+		return entropy;
+	}
+
+	public double logBase2(double number) {
+		return Math.log(number) / Math.log(2);
 	}
 
 	public String classify(Map<String, String> dataObject) {
